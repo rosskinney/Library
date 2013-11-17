564952fe8d4179a94767f19b47b5bebe5ee3abc9
MakerSquare Student <students@themakersquare.com>
2013-11-17 00:21:47 -0600

    Added codeto list checked out and avaaiable books

--- library.rb
+++ library.rb
@@ -10,10 +10,19 @@ class Library
   end
 
   def borrowed_books
-
+    @books.each do |book|
+      if book.status == "unavailable"
+         puts "#{book.title} by #{book.author} is checked out by #{book.borrower}"
+      end
+    end
   end
 
   def available_books
+    @books.each do |book|
+      if book.status == "available"
+        puts "#{book.title} by #{book.author} is currently available"
+      end
+    end
   end
 
   def add_book(book)
@@ -23,14 +32,10 @@ class Library
 
 
   def check_out(user, book)
-    puts "b4 doing anything: The following book #{book.title} has a status of #{book.status}" 
-    puts "b4 doing anything: user borrowed count = #{user.borrowed_books_count}"
     if book.status == "available" && user.borrowed_books_count < 2 
        user.borrowed_books_count +=1
        user.borrowed << book
        book.status = "unavailable"
-       puts "after: The following book #{book.title} has a status of #{book.status}" 
-       puts "after user borrowed count = #{user.borrowed_books_count}"
        puts "The following book #{book.title} has been checked out by #{user.name}"
     else
         puts "Sorry, this book is either unavailable or you have alread checkout out two books"
@@ -43,8 +48,6 @@ class Library
     puts "#{book.title} has has been returned by #{book.borrower.name}"
     book.borrower.borrowed.delete(book)
     book.borrower = nil
-
-
   end
 end
 
@@ -59,11 +62,13 @@ class Borrower
   end
 
   def borrowed_books
-
+    @borrowed.each do |book|
+        puts "#{book.title} by #{book.author}"
+    end
   end
 
   def borrowed_books_list
-    @borrowed.each do |book|
+   @borrowed.each do |book|
         puts "#{book.title} by #{book.author}"
     end
   end
@@ -71,7 +76,7 @@ end
 
 class Book
   attr_reader :title, :author
-  attr_accessor :status
+  attr_accessor :status ,:borrower
   def initialize(title, author)
       @title = title
       @author = author
