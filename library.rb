b65c2db345d3c3d4ec7fa4d3418c1b964467f5c2
MakerSquare Student <students@themakersquare.com>
2013-11-16 22:57:23 -0600

    Add logic to check_in method

--- library.rb
+++ library.rb
@@ -3,9 +3,6 @@ class Library
     @books = []
   end
 
-  # def books
-  # end
-
   def list_books
     @books.each do |book|
       puts "The book #{book.title} is in the library catalog with a the status of " "#{book.status}"
@@ -24,35 +21,51 @@ class Library
     puts "#{book.title} was added"
   end
 
+
   def check_out(user, book)
-    puts "The following book #{book.title} has been checked out by #{user.name}"
+    puts "b4 doing anything: The following book #{book.title} has a status of #{book.status}" 
+    puts "b4 doing anything: user borrowed count = #{user.borrowed_books_count}"
+    if book.status == "available" && user.borrowed_books_count < 2 
+       user.borrowed_books_count +=1
+       user.borrowed << book
+       book.status = "unavailable"
+       puts "after: The following book #{book.title} has a status of #{book.status}" 
+       puts "after user borrowed count = #{user.borrowed_books_count}"
+       puts "The following book #{book.title} has been checked out by #{user.name}"
+    else
+        puts "Sorry, this book is either unavailable or you have alread checkout out two books"
+    end
   end
 
   def check_in(book)
+    puts "check in method" 
+    book.status = "available"
+    puts "#{book.title} has has been returned by #{book.borrower.name}"
+    book.borrower.borrowed.delete(book)
+    book.borrower = nil
+
+
   end
 end
 
 class Borrower
-  attr_reader :name
+  attr_reader :name 
+  attr_accessor :borrowed_books_count, :borrowed
   def initialize(name)
     @name = name
     puts "#{name} was created "
+    @borrowed_books_count =  0
+    @borrowed = []
   end
 
   def borrowed_books
-  end
 
-  # def name
-  #   @name.each do |name|
-  #     puts book.title
-  #   end
-
-  # end
-
-  def borrowed_books_count
   end
 
   def borrowed_books_list
+    @borrowed.each do |book|
+        puts "#{book.title} by #{book.author}"
+    end
   end
 end
 
@@ -64,5 +77,6 @@ class Book
       @author = author
       @status = "available"
       @borrower = nil
+
   end
 end
